c1130ec6c93db0fb37494dfba7b977067f2091c0
Ignacio Fontalvo <ifontalvo@conversantmedia.com>
2018-09-07 08:35:00 -0500

    cron testing

---
+++ script.js
@@ -0,0 +1,72 @@
+const cron = require('cron');
+
+const audiences = [{
+  name: 'audiences_one',
+  status: 'Ready',
+  scheduled: '2018-09-06'
+},
+{
+  name: 'audiences_two',
+  status: 'InProgress'
+}];
+
+
+const processAudience = (...args) => {
+  setTimeout(() => {
+    audiences.push({
+      name: args[0],
+      status: args[1],
+      scheduled: args[2]
+    });
+  }, args[3]);
+}
+
+processAudience('audience_three', 'Ready', '2018-09-06', 15000);
+processAudience('audience_four', 'Ready', '2018-09-06', 30000);
+processAudience('audience_five', 'InProgress', '', 10000);
+processAudience('audience_six', 'Ready', '2018-09-06', 40000);
+
+const fetchAudiences = () => {
+  return audiences.filter(x => (x.status !== 'InProgress' && x.scheduled));
+}
+
+const activateAudiences = (audience) => {
+  audience.status = 'InProgress';
+}
+
+const getDate = (string) => {
+  const date = new Date(string);
+  const month = (date.getMonth() + 1).toString().length < 2 ? '0' + (date.getMonth() + 1).toString() : (date.getMonth() + 1).toString();
+  const day = date.getDate().toString().length < 2 ? '0' + date.getDate().toString() : date.getDate().toString();
+  const year = date.getFullYear();
+  return `${year}-${month}-${day}`;
+}
+
+let i = 0;
+const job = new cron.CronJob('*/10 * * * * *', function () {
+  console.log(`${++i} hour running...`);
+  const date = new Date();
+  console.log(`fetching Audiences ...`);
+  const pendindAudiences = fetchAudiences();
+  console.log(pendindAudiences);
+  const formatDate = getDate(date.toDateString());
+  pendindAudiences.forEach(element => {
+    console.log(`Scheduled date ${element.scheduled}`);
+    console.log(`Today date ${formatDate}`);
+    if (element.scheduled === formatDate) {
+      console.log(`Activating Audience ...`);
+      activateAudiences(element);
+    } else {
+      console.log(`Audience not able to activate ...`);
+    }
+  });
+  console.log(`********************** End Activation ******************************`);
+  console.log(`Audiences Array`);
+  console.log(audiences);
+  console.log(`*******************************************************************`);
+  console.log(`********************** End Iteration ******************************`);
+  console.log(`*******************************************************************`);
+});
+
+job.start();
+
